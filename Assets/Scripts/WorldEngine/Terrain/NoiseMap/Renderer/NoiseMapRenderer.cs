a7acd87a1d0f96526e8072145c9a5d943e9aea1e
Brandon Hafenrichter <bhafenri@gmail.com>
2020-04-10 17:26:07 +0200

    Added Blurred Texture

--- Assets/Scripts/WorldEngine/Terrain/NoiseMap/Renderer/NoiseMapRenderer.cs
+++ Assets/Scripts/WorldEngine/Terrain/NoiseMap/Renderer/NoiseMapRenderer.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-
+using System;
 public class NoiseMapRenderer : MonoBehaviour
 {
+  public int COLOR_PADDING = 1;
   internal Texture2D BuildTexture(TerrainType[] terrainConfigs, AnimationCurve heightCurve, float[,] heightMap)
   {
     int tileDepth = heightMap.GetLength(0);
@@ -24,10 +25,8 @@ public class NoiseMapRenderer : MonoBehaviour
     int tileWidth = heightMap.GetLength(1);
 
     Color[] colorMap = new Color[tileDepth * tileWidth];
-    for (int zIndex = 0; zIndex < tileDepth; zIndex++)
-    {
-      for (int xIndex = 0; xIndex < tileWidth; xIndex++)
-      {
+    for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
+      for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
         // transform the 2D map index is an Array index
         int colorIndex = zIndex * tileWidth + xIndex;
         float height = heightCurve.Evaluate(heightMap[zIndex, xIndex]);
@@ -38,12 +37,42 @@ public class NoiseMapRenderer : MonoBehaviour
             break;
           }
         }
-
         // colorMap[colorIndex] = Color.Lerp(Color.white, Color.black, heightCurve.Evaluate(heightMap[zIndex, xIndex]));
+      }
+    }
+
+    // average out the colors compared to its neighbors
+    for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
+      for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
+        float redAvg = 0;
+        float greenAvg = 0;
+        float blueAvg = 0;
+
+        for (var i = 0; i < COLOR_PADDING; i++) {
+          try {
+            addColor(colorMap[(zIndex) * tileWidth + (xIndex)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex + COLOR_PADDING) * tileWidth + (xIndex)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex - COLOR_PADDING) * tileWidth + (xIndex)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex) * tileWidth + (xIndex + COLOR_PADDING)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex) * tileWidth + (xIndex - COLOR_PADDING)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex - COLOR_PADDING) * tileWidth + (xIndex - COLOR_PADDING)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex + COLOR_PADDING) * tileWidth + (xIndex + COLOR_PADDING)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex - COLOR_PADDING) * tileWidth + (xIndex + COLOR_PADDING)], ref redAvg, ref greenAvg, ref blueAvg);
+            addColor(colorMap[(zIndex + COLOR_PADDING) * tileWidth + (xIndex - COLOR_PADDING)], ref redAvg, ref greenAvg, ref blueAvg);
+
+            colorMap[(zIndex) * tileWidth + (xIndex)] = new Color(redAvg / 9, greenAvg / 9, blueAvg / 9, 1);
+          } catch (Exception e) {}
+        }
       }
     }
     
     return colorMap;
+  }
+
+  private void addColor(Color color, ref float red, ref float green, ref float blue) {
+    red += color.r;
+    green += color.g;
+    blue += color.b;
   }
 }
 
